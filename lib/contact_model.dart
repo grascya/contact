1669cb185797bdadd142c87682c3f0a7b252564b
sophiazack <sophiazakari717@gmail.com>
2021-12-13 16:47:11 +0000

    first commit

---
+++ lib/contact_model.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+Contact contactFromJson(String str) => Contact.fromJson(json.decode(str));
+
+String contactToJson(Contact data) => json.encode(data.toJson());
+
+class Contact {
+  Contact({
+    required this.name,
+    required this.phone,
+    required this.email,
+    required this.country,
+    required this.region,
+    required  this.picture
+  });
+
+  String name;
+  String phone;
+  String email;
+  String country;
+  String region;
+  String picture;
+
+  factory Contact.fromJson(Map<String, dynamic> json) => Contact(
+        name: json["name"],
+        phone: json["phone"],
+        email: json["email"],
+        country: json["country"],
+        region: json["region"],
+           picture: json["picture"]
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "phone": phone,
+        "email": email,
+        "country": country,
+        "region": region,
+           "picture": picture,
+      };
+}
